dd573a162c047a2bff71f9bd1471582204ae6c48
michaelkharadze <miqaelkharadze@gmail.com>
2022-05-30 16:57:21 +0400

    added build

---
+++ build/precache-manifest.cdbaca55440abbfec00d8ec82a547ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fc12e8492346fa135f159b9eb7e872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43ac849b09118d9926eb",
+    "url": "/static/css/main.ee3cf3e2.chunk.css"
+  },
+  {
+    "revision": "e3483eba663064abe6da",
+    "url": "/static/js/2.415215b2.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.415215b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ac849b09118d9926eb",
+    "url": "/static/js/main.76db8cbc.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  }
+]);
