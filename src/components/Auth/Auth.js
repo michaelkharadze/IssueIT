3b2e2555283a4033dcb128574830431d9ab586c5
michaelkharadze <miqaelkharadze@gmail.com>
2022-05-30 17:07:09 +0400

    Fixed google login URL permissions

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -79,7 +79,7 @@ const SignUp = () => {
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin
-            clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
+            clientId="195858286713-3rqsqolooqfja20kjcldllh01roiml71.apps.googleusercontent.com"
             render={(renderProps) => (
               <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
